02263ebd653ebb8b51aab7c540a8d55a0c28c5aa
Mikhael Mourao <140970147+mikhaelmourao@users.noreply.github.com>
2025-05-21 16:31:22 -0300

    project created

---
+++ src/main/java/com/devbackendjava/intensivoSpring2/IntensivoSpring2Application.java
@@ -0,0 +1,13 @@
+package com.devbackendjava.intensivoSpring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntensivoSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntensivoSpring2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devbackendjava/intensivoSpring2/IntensivoSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbackendjava.intensivoSpring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntensivoSpring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
